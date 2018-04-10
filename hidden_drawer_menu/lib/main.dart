bb7c6270740cd31be9d97e52c3a9f6874ce3b8c1
Eduardo <eduardoshibuka@gmail.com>
2018-04-10 22:59:33 +0000

    Fazendo zoom  scaffold

--- hidden_drawer_menu/lib/main.dart
+++ hidden_drawer_menu/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/menu_screen.dart';
 import 'package:hidden_drawer_menu/other_screen.dart';
 import 'package:hidden_drawer_menu/restaurant_screen.dart';
+import 'package:hidden_drawer_menu/zoom_scaffold.dart';
 
 void main() => runApp(new MyApp());
 
@@ -25,146 +27,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var activeScreen = otherScreen;
+  var activeScreen = restaurantScreen;
 
   @override
   Widget build(BuildContext context) {
-    return new Stack(
-      children: [          
-        createMenuScreen(),
-        //createContentDisplay()
-        
-      ]      
-    );
-  }
-
-  Widget createMenuItems() {
-    return new Transform(
-      transform: new Matrix4.translationValues(
-        0.0, 
-        225.0,
-        0.0
-      ),
-      child: new Column(
-        children: <Widget>[
-          new _MenuListItem(
-            title: 'THE PADDOCK',
-            isSelected: true,
-          ),
-          new _MenuListItem(
-            title: 'THE HERO',
-            isSelected: false,
-          ),
-          new _MenuListItem(
-            title: 'HELP US GROWN',
-            isSelected: false,
-          ),
-          new _MenuListItem(
-            title: 'SETTINGS',
-            isSelected: false,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget createMenuTitle() {
-    return new Transform(
-      transform: new Matrix4.translationValues(-100.0, 0.0, 0.0),
-      child: new OverflowBox(
-        maxWidth: double.infinity,
-        alignment: Alignment.topLeft,
-        child: new Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: new Text(  
-            'Menu',
-            style: new TextStyle(
-              color: const Color(0x88444444),
-              fontSize: 240.0,
-              fontFamily: 'mermaid'
-            ),
-            textAlign: TextAlign.left,
-            softWrap: false,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget createMenuScreen(){
-    return new Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: new BoxDecoration(
-        image: new DecorationImage(
-          image: new AssetImage('assets/dark_grunge_bk.jpg'),
-          fit: BoxFit.cover
-        )            
-      ), 
-      child: new Material(
-        color: Colors.transparent,
-        child: new Stack(
-          children: <Widget>[
-            createMenuTitle(),
-            createMenuItems()
-          ],
-        ),
-      ),         
-    );
-  }
-
-  Widget createContentDisplay () {
-    return new Container(
-      decoration: new BoxDecoration(image: activeScreen.background),
-      child: new Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: new AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            leading: new IconButton(
-              icon: new Icon(Icons.menu),
-              onPressed: () {},
-            ),
-            title: new Text(
-              activeScreen.title,
-              style: new TextStyle(fontFamily: 'bebas-neue', fontSize: 25.0),
-            ),
-          ),
-          body: activeScreen.contentBuilder(context)),
-    );
-  }
-}
-
-class _MenuListItem extends StatelessWidget {
-  final String title;
-  final bool isSelected;
-
-  _MenuListItem({
-    this.title,
-    this.isSelected
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return new InkWell(
-      splashColor: const Color(0X44000000),
-      onTap: isSelected ? null : () {} ,
-      child: new Container(
-        width: double.infinity,
-        child: new Padding(
-          padding: const EdgeInsets.only(left: 50.0, top: 15.0, bottom: 15.0),
-          child: new Text(
-            this.title,
-            style: new TextStyle(
-              color: isSelected ? Colors.red : Colors.white,
-              fontSize: 20.0,
-              fontFamily: 'bebas-neeue',
-              letterSpacing: 2.0
-            ),
-          ),
-        ),
-      ),
+    return new ZoomScaffold(
+      menuScreen: new MenuScreen(),
+      contentScreen: activeScreen,
     );
   }
 }
--- hidden_drawer_menu/lib/zoom_scaffold.dart
+++ hidden_drawer_menu/lib/zoom_scaffold.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hidden_drawer_menu/menu_screen.dart';
 
 class Screen {
   final String title;
@@ -6,4 +8,78 @@ class Screen {
   final WidgetBuilder contentBuilder;
 
   Screen({this.title, this.background, this.contentBuilder});
+}
+
+class ZoomScaffold extends StatefulWidget {
+  final Widget menuScreen;
+  final Screen contentScreen;
+
+  ZoomScaffold({
+    this.menuScreen,
+    this.contentScreen,
+  });
+
+  @override  
+  _ZoomScaffoldState createState() => new _ZoomScaffoldState();
+}
+
+class _ZoomScaffoldState extends State<ZoomScaffold> {
+    Widget createContentDisplay () {
+    return zoomAndSlideContent(
+      new Container(
+      decoration: new BoxDecoration(image: widget.contentScreen.background),
+      child: new Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: new AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            leading: new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            title: new Text(
+              widget.contentScreen.title,
+              style: new TextStyle(fontFamily: 'bebas-neue', fontSize: 25.0),
+            ),
+          ),
+          body: widget.contentScreen.contentBuilder(context)),
+      )
+    );
+  }
+
+  zoomAndSlideContent(Widget content) {
+    return new Transform(
+      transform: new Matrix4
+        .translationValues(275.0, 0.0, 0.0)
+        ..scale(0.8, 0.8),
+        alignment: Alignment.centerLeft,        
+      child: new Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: const Color(0X44000000),
+              offset: const Offset(0.0, 0.5),
+              blurRadius: 20.0,
+              spreadRadius: 10.0,
+            )
+          ]
+        ),
+        child: new ClipRRect(
+          borderRadius: new BorderRadius.circular(10.0), 
+          child: content
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: [          
+        widget.menuScreen,
+        createContentDisplay()        
+      ]      
+    );
+  }
 }
